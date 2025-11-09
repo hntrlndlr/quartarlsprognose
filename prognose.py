f80097b7303f57e4de843551918bf51171442a7e
hntrlndlr <123324346+hntrlndlr@users.noreply.github.com>
2025-11-09 20:54:53 +0100

    Update prognose.py

--- prognose.py
+++ prognose.py
@@ -456,6 +456,10 @@ with tabs[0]:
                     st.session_state.last_button_click = None
                     st.session_state.selected_event = None
                     st.rerun()
+                if st.form_submit_button("Abbrechen"):
+                    st.session_state.last_button_click = None
+                    st.session_state.selected_event = None
+                    st.rerun()
         else:
             klient_id = title.split(" - ")[0].strip()
     
